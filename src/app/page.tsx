b8c403aea803b98f4cc11c23d9b15fd269bd2251
Gen <ichihashigen@gmail.com>
2024-04-08 00:52:50 -0400

    huh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import FAQ from "./components/FAQ";
 import Sponsor from "./components/sponsor";
 import Footer from "./components/footer";
 import "./globals.css";
-import AboutGeeseHacks from "./components/terminal";
 import Schedule from "./components/schedule";
 
 export default function Home() {
